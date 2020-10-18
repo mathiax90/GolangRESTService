452967b1d4a428f90f20e6cdd7327566f5caa1a9
mathiax90 <mathiax90@gmail.com>
2020-10-18 23:22:59 +0800

    create xml feature. Add scanner for SimpleDate type.

--- SimpleDateTime.go
+++ SimpleDateTime.go
@@ -6,6 +6,8 @@ import (
 	//"fmt"
     "bytes"
     "encoding/xml"
+    "errors"
+    "database/sql/driver"
 )
 
 type SimpleDate struct {
@@ -30,7 +32,19 @@ func (simpleDate SimpleDate) MarshalXML(e *xml.Encoder, start xml.StartElement) 
     return e.EncodeElement(simpleDate.Time.Format("2006-01-02"), start)
 }
 
-
-// func (simpleDate *SimpleDate) Scan(value interface{}) error {
-
-// }
+func (simpleDate *SimpleDate) Scan(value interface{}) error {    
+    if value == nil {
+        return errors.New("SimpleDate can't Scan null use NullSimpleDate instead")
+    } else {
+        if DateAsString, err := driver.String.ConvertValue(value); err == nil {            
+            if v, ok := DateAsString.(string); ok {                
+                simpleDate.Time, err = time.Parse("2006-01-02 15:04:05 -0700 MST", string(v))
+                if err != nil {
+                    return err
+                }
+                return nil
+            }
+        }
+        return errors.New("failed to scan SimpleDate")
+    }
+}
