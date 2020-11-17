0bb21b2be3f099374fad6c4308ffc5cf1624534c
mathiax90 <mathiax90@gmail.com>
2020-11-18 01:34:21 +0800

    add cors

--- main.go
+++ main.go
@@ -16,6 +16,10 @@ import (
 	"github.com/jmoiron/sqlx"
 	"github.com/joho/godotenv"
 	_ "github.com/jackc/pgx/stdlib"
+	"github.com/rs/cors"
+	"strings"
+	"io"
+	"bytes"
 )
 
 func init() {
@@ -60,11 +64,42 @@ func main() {
 	
 	fmt.Println("service start")
 	router := mux.NewRouter()
+	c := cors.New(cors.Options{
+		AllowedOrigins: []string{"*"},                            // All origins
+		AllowedMethods: []string{"POST", "GET", "PUT", "DELETE"}, // Allowing only get, just an example
+	})
+
 	router.HandleFunc("/orders/", createOrderHandler).Methods("POST")
+	router.HandleFunc("/orders/", getOrderHandler).Methods("GET")
 	router.HandleFunc("/orders/idpacs", getIdPacsHandler).Methods("GET")
-	
+	router.HandleFunc("/files/upload", UploadFile).Methods("POST")
 	s := http.StripPrefix("/ReestrFileStorage/", http.FileServer(http.Dir("./ReestrFileStorage/")))
 	router.PathPrefix("/ReestrFileStorage/").Handler(s)
-    log.Fatal(http.ListenAndServe(":8000", router))
+    log.Fatal(http.ListenAndServe(":8000", c.Handler(router)))
 }
 
+func UploadFile(w http.ResponseWriter, r *http.Request){
+	r.ParseMultipartForm(32 << 20) // limit your max input length!
+	var buf bytes.Buffer	
+    // in your case file would be fileupload
+    file, header, err := r.FormFile("file1")
+    if err != nil {
+        panic(err)
+    }
+    defer file.Close()
+    name := strings.Split(header.Filename, ".")
+    fmt.Printf("File name %s\n", name[0])
+    // Copy the file data to my buffer
+    io.Copy(&buf, file)
+    // do something with the contents...
+    // I normally have a struct defined and unmarshal into a struct, but this will
+    // work as an example
+    contents := buf.String()
+    fmt.Println(contents)
+    // I reset the buffer in case I want to use it again
+    // reduces memory allocations in more intense projects
+    buf.Reset()
+    // do something else
+    // etc write header
+    return
+}
